4724bb208cef7e74b3324c01f67650b1d6d87d28
Lucas Viotti <lucasviotti@iresm.edu.ar>
2023-10-15 21:23:44 -0300

    Some changes on index file.

--- nanoshop-it/api/index.js
+++ nanoshop-it/api/index.js
@@ -15,9 +15,8 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 
-// Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
   server.listen(3000, () => {
-    console.log("Conectado al puerto 3000"); // eslint-disable-line no-console
+    console.log("Conectado al puerto 3000");
   });
 });
