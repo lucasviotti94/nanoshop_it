784c42b84c94e92c5db3e3ff89454a50ddbe70fd
Lucas Viotti <lucasviotti@iresm.edu.ar>
2023-10-18 18:34:22 -0300

    Big improoves on NavBar.

---
+++ nanoshop-it/client/src/components/Home/Home.jsx
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import "./home.css";
+
+export default function Home() {
+    return (
+        <div className='home'>
+            <div>OLA</div>
+            <div>OLA</div>
+            <div>OLA</div>
+            <div>OLA</div>
+            <div>OLA</div>
+            <div>OLA</div>
+            <div>OLA</div>
+            <div>OLA</div>
+            <div>OLA</div>
+            <div>OLA</div>
+            <div>OLA</div>
+            <div>OLA</div>
+            <div>OLA</div>
+            <div>OLA</div>
+            <div>OLA</div>
+            <div>OLA</div>
+            <div>OLA</div>
+            <div>OLA</div>
+            <div>OLA</div>
+            <div>OLA</div>
+            <div>OLA</div>
+            <div>OLA</div>
+            <div>OLA</div>
+            <div>OLA</div>
+            <div>OLA</div>
+            <div>OLA</div>
+            <div>OLA</div>
+            <div>OLA</div>
+            <div>OLA</div>
+            <div>OLA</div>
+            <div>OLA</div>
+            <div>OLA</div>
+            <div>OLA</div>
+            <div>OLA</div>
+            <div>OLA</div>
+            <div>OLA</div>
+            <div>OLA</div>
+            <div>OLA</div>
+            <div>OLA</div>
+        </div>
+
+        );
+    }
+
+
+    
+  
